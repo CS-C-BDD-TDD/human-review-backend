c4a5bfc1b8aa917a90130ab65f88381e7e27df12
Kiet Tran <drkiettran@gmail.com>
2018-10-09 11:28:37 -0400

    Added HumanreviewApiControllerTest, gitignore, updated pom to include sonarqube scan & unit tests

--- src/main/java/gov/dhs/nppd/humanreview/service/HumanreviewApiController.java
+++ src/main/java/gov/dhs/nppd/humanreview/service/HumanreviewApiController.java
@@ -1,8 +1,12 @@
 package gov.dhs.nppd.humanreview.service;
 
+import java.util.Optional;
+
 import org.openapitools.api.HumanreviewApi;
 import org.openapitools.model.HumanReviewItem;
 import org.openapitools.model.ListOfHumanReviewItems;
+import org.openapitools.repository.HumanreviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,40 +18,45 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-import java.util.Optional;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2018-09-28T12:48:27.866-04:00[America/New_York]")
 
 @Controller
 @RequestMapping("${openapi.humanReview.base-path:/api/v1}")
 public class HumanreviewApiController implements HumanreviewApi {
 
-    private final NativeWebRequest request;
+	private final NativeWebRequest request;
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public HumanreviewApiController(NativeWebRequest request) {
-        this.request = request;
-    }
+	@Autowired
+	private HumanreviewRepository hrRepo;
 
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
-    
-    @Override
+	@org.springframework.beans.factory.annotation.Autowired
+	public HumanreviewApiController(NativeWebRequest request) {
+		this.request = request;
+	}
+
+	@Override
+	public Optional<NativeWebRequest> getRequest() {
+		return Optional.ofNullable(request);
+	}
+
+	@Override
 	@ApiOperation(value = "", nickname = "humanreviewPendingGet", notes = "", response = ListOfHumanReviewItems.class, tags = {})
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ListOfHumanReviewItems.class) })
 	@RequestMapping(value = "/humanreview/pending", produces = { "application/json" }, method = RequestMethod.GET)
 	public ResponseEntity<ListOfHumanReviewItems> humanreviewPendingGet() {
-		System.out.println(" entering ....");
-
-		ListOfHumanReviewItems listOfHumanReviewItems = new ListOfHumanReviewItems();
-		HumanReviewItem hrItem1 = new HumanReviewItem();
-		hrItem1.setStixId(42412345);
-
-		listOfHumanReviewItems.add(hrItem1);
+		ListOfHumanReviewItems listOfHumanReviewItems = hrRepo.getPending();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Content-type", "application/json");
 		return ResponseEntity.accepted().headers(headers).body(listOfHumanReviewItems);
 	}
 
+	public HumanreviewRepository getHrRepo() {
+		return hrRepo;
+	}
+
+	public void setHrRepo(HumanreviewRepository hrRepo) {
+		this.hrRepo = hrRepo;
+	}
+
+	
 }
