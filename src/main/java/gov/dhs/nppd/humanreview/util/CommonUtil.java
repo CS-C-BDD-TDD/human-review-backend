7198913facd43975589a049c6a580d0d1cd55f8b
dbrletic <dbrletic@redhat.com>
2018-10-18 12:26:47 -0400

    DHS-65 Removed getting the application.properties for TokenTimeout (property is not being pulled in correctly for testing) and added a test application.properties. Added additional logging

--- src/main/java/gov/dhs/nppd/humanreview/util/CommonUtil.java
+++ src/main/java/gov/dhs/nppd/humanreview/util/CommonUtil.java
@@ -14,8 +14,8 @@ public class CommonUtil {
 	@Autowired
 	AuthCredentialsRepository authCredentialsRepository;
 
-	@Value("${gov.dhs.nppd.tokenTimeout}")
-	private long tokenTimeout;
+	//@Value("${gov.dhs.nppd.tokenTimeout}")
+	private long tokenTimeout = 1800000;
 
 	public boolean tokenValidator(String token) {
 		System.out.println("Current Token to validate: " + token);
@@ -25,9 +25,18 @@ public class CommonUtil {
 
 		AuthCredentials tokenCheck = authCredentialsRepository.findByToken(token);
 
+		
+		
 		Date afterAddingMins = new Date(tokenCheck.getDate().getTime() + tokenTimeout);
 		Date now = new Date();
 
+		
+		System.out.println("****************************");
+		System.out.println("Current Time: " + now);
+		System.out.println("Token Login Time: " + tokenCheck.getDate()); 
+		System.out.println("Token Expire Time: " + afterAddingMins );
+		System.out.println("****************************");
+		
 		if (tokenCheck != null && !tokenCheck.getDate().after(now) && !afterAddingMins.before(now)) {
 			return true;
 		} else {
